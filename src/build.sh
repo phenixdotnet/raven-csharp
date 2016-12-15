7c6517cdab35936922d1e4df53e5d2b2ba964ec9
Vincent Lainé <vlaine@digitaleo.com>
2016-12-15 10:06:24 +0100

    Fix build.sh script to move to it directory before running

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     $( cd -P "$( dirname "$SOURCE" )" )
+     pwd
+}
+
+cd "$(get_script_dir)"
+
 echo "Downloading Cake bootstrap script"
 curl -Lsfo buildcake.sh http://cakebuild.net/bootstrapper/linux
 chmod u+x ./buildcake.sh
